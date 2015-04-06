357fefd6e39ab289af6c3ff07c060acd5043fabf
lifeofpentester <root@localhost.localdomain>
2015-04-07 04:19:35 +0530

    First commit

---
+++ Cryptography/Net-Force_302_Substituion_Solution.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+"""Solution to Net-Force Cryptography challenge 302: Substitution....
+   --by Pranshu Bajpai"""
+
+def subs(x):
+      return {
+          "o": "l",
+	  "g": "e",
+	  "r": "u",
+	  "t": "z",
+	  "z": "t",
+	  "d": "f",
+	  "l":"o",
+	  "u":"r",
+	  "a":"c",
+	  "e":"g",
+	  "d":"f",
+	  "c":"a"
+          }[x]
+
+CIPHERTEXT = "ogrk hg tl`n srbszizrzig aiphgu mg ycahzylluf vllu fg ahcoogneg pceinc is ngzdluag"
+
+PLAINTEXT = ""
+
+for LETTER in CIPHERTEXT:
+    if LETTER=='o' or LETTER=='g' or LETTER=='r' or LETTER=='t' or LETTER=='z' or LETTER=='d' or LETTER=="l" or LETTER=="u" or LETTER=="a" or LETTER=="e" or LETTER=="d" or LETTER=='c':
+       PLAINTEXT = PLAINTEXT + subs(LETTER)
+    else:
+       PLAINTEXT = PLAINTEXT + LETTER
+
+print PLAINTEXT
+		
